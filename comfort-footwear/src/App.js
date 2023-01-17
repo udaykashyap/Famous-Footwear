758bb3d145b427b89a0fb19223e36231dd515815
udaykashyap <udaykashyap.hjp@gmail.com>
2023-01-17 18:22:59 +0530

    dummy created

---
+++ comfort-footwear/src/App.js
@@ -0,0 +1,15 @@
+
+import Footwear from './Footwear.png';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Comfort Footwear</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
