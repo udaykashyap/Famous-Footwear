b741b1ac0a4b26de8bb2f61ada4d3121fe66ff3a
udaykashyap <udaykashyap.hjp@gmail.com>
2023-01-18 15:35:55 +0530

    navbar basic structure done

--- comfort-footwear/src/App.js
+++ comfort-footwear/src/App.js
@@ -1,12 +1,11 @@
 
-import Footwear from './Footwear.png';
 import './App.css';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Comfort Footwear</h1>
+
       <Navbar />
     </div>
   );
